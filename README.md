bd33e28cc33ffc59a5e9cc80de81ebe95b71d210
gnosis23 <bj050323@gmail.com>
2018-10-13 01:21:51 +0800

    core

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # react-tiny-scroll
-A simple React component that scrolls slowly.
+A simple React component that prevents scrolling of parent element when inner element scroll position reaches top/bottom
+
+## Installation
+Using npm:
+```
+$ npm install --save react-tiny-scroll
+```
+
+## Usage
+```js
+import { TinyScroll } from 'react-tiny-scroll'
+
+render() {
+  return (
+    <TinyScroll>
+      <span>Contents</span>
+    </TinyScroll>
+  );
+}
+```
--- test/TestLayout.js
+++ test/TestLayout.js
@@ -1,14 +1,52 @@
 import React from 'react';
 import TinyScroll from '../lib/scroll';
 
+const styleParent = {
+  border: '1px solid #e3e4e5', 
+  height: 400, 
+  width: 200,
+  margin: '',
+  overflowY: 'auto'
+};
+const styleChild = {
+  border: '1px solid #0098df', 
+  height: 200, 
+  overflowY: 'auto'
+};
+
 export default class TestLayout extends React.Component {
 
   render() {
     return (
-      <div>
+      <div style={styleParent}>
+        <p>1111</p>
+        <p>1111</p>
+        <p>1111</p>
         <TinyScroll>
-          <p>Hello world</p>
+          <div style={styleChild}>
+            <p>Hello world</p>
+            <p>Hello world</p>
+            <p>Hello world</p>
+            <p>Hello world</p>
+            <p>Hello world</p>
+            <p>Hello world</p>
+          </div>
         </TinyScroll>
+        <p>1111</p>
+        <p>1111</p>
+        <p>1111</p>
+        <p>1111</p>
+        <p>1111</p>
+        <p>1111</p>
+        <p>1111</p>
+        <p>1111</p>
+        <p>1111</p>
+        <p>1111</p>
+        <p>1111</p>
+        <p>1111</p>
+        <p>1111</p>
+        <p>1111</p>
+        <p>1111</p>
       </div>
     );
   }
